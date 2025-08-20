223c9ba687851c97121efd79373aaa88cd6bbe16
NH <mnh4140@gmail.com>
2025-08-20 18:02:12 +0900

    [Env] 프로젝트 초기 셋팅

---
+++ CoreDataAuth/CoreDataAuth/StartViewController.swift
@@ -0,0 +1,14 @@
+//
+//  StartViewController.swift
+//  CoreDataAuth
+//
+//  Created by NH on 8/20/25.
+//
+
+import UIKit
+
+final class StartViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .cyan
+    }
+}
