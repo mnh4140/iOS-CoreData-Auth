f53842a2571f453906e7ca90a7d887da9857a7d7
NH <mnh4140@gmail.com>
2025-08-21 18:32:58 +0900

    [Chore] #4 rootVC 변경 테스트용 코드 추가

--- CoreDataAuth/CoreDataAuth/App/SceneDelegate.swift
+++ CoreDataAuth/CoreDataAuth/App/SceneDelegate.swift
@@ -5,6 +5,20 @@
 //  Created by NH on 8/20/25.
 //
 
+enum views {
+    case start
+    case welcome
+    
+    var vc: UIViewController {
+        switch self {
+        case .start:
+            return StartViewController()
+        case .welcome:
+            return WelcomeViewController()
+        }
+    }
+}
+
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -13,13 +27,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        window.rootViewController = StartViewController()
+        window.rootViewController = views.welcome.vc
         window.makeKeyAndVisible()
         
         self.window = window
